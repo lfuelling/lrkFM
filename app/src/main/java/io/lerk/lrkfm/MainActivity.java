bb505269eca1650f12388f8641c0232c16f7acf1
Lukas Fülling <lukas@k40s.net>
2017-03-14 02:27:01 +0100

    [#4] fix build

--- app/src/main/java/io/lerk/lrkfm/MainActivity.java
+++ app/src/main/java/io/lerk/lrkfm/MainActivity.java
@@ -75,17 +75,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        if (id == R.id.nav_home) {
+            // Handle the home action
+        } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_todo) {
+
+        } else if (id == R.id.nav_view) {
 
         }
 
