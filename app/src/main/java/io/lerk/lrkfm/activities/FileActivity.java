5f8776056a6626f3108c6b362854d36dfe9f3a5f
Lukas Fülling <lukas@k40s.net>
2017-03-19 21:26:27 +0100

    launch settings on menu select

--- app/src/main/java/io/lerk/lrkfm/activities/FileActivity.java
+++ app/src/main/java/io/lerk/lrkfm/activities/FileActivity.java
@@ -98,7 +98,8 @@ public class FileActivity extends AppCompatActivity
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        //noinspection deprecation
+        drawer.setDrawerListener(toggle); // I ned dis
         toggle.syncState();
     }
 
@@ -219,7 +220,6 @@ public class FileActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
@@ -230,8 +230,7 @@ public class FileActivity extends AppCompatActivity
         } else if (id == R.id.nav_path) {
             promptAndLoadPath();
         } else if (id == R.id.nav_settings) {
-            Intent i = new Intent(this, SettingsActivity.class);
-            startActivity(i);
+            launchSettings(item);
         } else if (id == R.id.nav_share) {
             ApplicationInfo app = getApplicationContext().getApplicationInfo();
             String filePath = app.sourceDir;
@@ -262,6 +261,11 @@ public class FileActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void launchSettings(MenuItem item) {
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
     }
 
     private void promptAndLoadPath() {
@@ -302,5 +306,4 @@ public class FileActivity extends AppCompatActivity
             return false;
         }
     }
-
 }
