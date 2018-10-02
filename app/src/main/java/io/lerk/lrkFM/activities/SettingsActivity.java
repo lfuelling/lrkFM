cd520fccacdb079a8cb80ce60ea6de993e4398bb
Lukas Fülling <lukas@k40s.net>
2018-10-03 00:10:04 +0200

    remove unused stuff

--- app/src/main/java/io/lerk/lrkFM/activities/SettingsActivity.java
+++ app/src/main/java/io/lerk/lrkFM/activities/SettingsActivity.java
@@ -82,15 +82,6 @@ public class SettingsActivity extends ThemedAppCompatPreferenceActivity {
                 Log.e(TAG, "Unable to get App version!", e);
             }
 
-            Preference context_for_ops_toast = findPreference("context_for_ops_toast");
-            SwitchPreference context_for_ops = (SwitchPreference) findPreference("context_for_ops");
-
-            context_for_ops_toast.setEnabled(context_for_ops.isChecked());
-            context_for_ops.setOnPreferenceChangeListener((preference, newValue) -> {
-                context_for_ops_toast.setEnabled((Boolean) newValue);
-                return true;
-            });
-
             addOnPreferenceChangeListeners(this.getPreferenceScreen());
         }
 
