822f5f2499f36915f28109d1be01f8293d82ead3
Lukas Fülling <lukas@k40s.net>
2019-09-25 19:34:36 +0200

    add nullcheck for vibrator

--- app/src/main/java/io/lerk/lrkFM/VibratingToast.java
+++ app/src/main/java/io/lerk/lrkFM/VibratingToast.java
@@ -3,11 +3,12 @@ package io.lerk.lrkFM;
 import android.content.Context;
 import android.os.Vibrator;
 import android.widget.Toast;
+
 import io.lerk.lrkFM.consts.PreferenceEntity;
 
 /**
  * Vibrating Toast.
- *
+ * <p>
  * Based on: https://stackoverflow.com/a/6109644/1979736
  *
  * @author Lukas Fülling (lukas@k40s.net)
@@ -16,8 +17,11 @@ public class VibratingToast extends Toast {
 
     public VibratingToast(Context context, CharSequence text, int duration) {
         super(context);
-        if(new Pref<Boolean>(PreferenceEntity.VIBRATING_TOASTS).getValue()) {
-            ((Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(120);
+        if (new Pref<Boolean>(PreferenceEntity.VIBRATING_TOASTS).getValue()) {
+            Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            if (vibrator != null) {
+                vibrator.vibrate(120);
+            }
         }
         makeText(context, text, duration).show();
     }
